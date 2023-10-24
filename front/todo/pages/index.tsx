eb2fb3c0e95e4593bbe2b31c835785cbf5b4ab12
me990928 <me990928@gmail.com>
2023-10-24 14:59:13 +0900

    Create Nav Bar

--- front/todo/pages/index.tsx
+++ front/todo/pages/index.tsx
@@ -1,12 +1,14 @@
 import { Inter } from 'next/font/google'
 import LoginPage from './loginpage'
+import NavBar from './components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <LoginPage />
+      {/* <LoginPage /> */}
+      <NavBar />
     </>
   )
 }
