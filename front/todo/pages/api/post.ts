bc0fbcd4181beda0f8d841113c5835cc31cdf4b2
YokotaFumiya <yokotafumiya243182@gmail.com>
2023-10-26 12:26:02 +0900

    廣瀬君が書いてくれたmockpost.tsをそのままコピペ

--- front/todo/pages/api/post.ts
+++ front/todo/pages/api/post.ts
@@ -1,41 +1,50 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Ewert } from 'next/font/google';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
-    if (req.method !== "POST") {
-        return new Response(null, { status: 400, statusText: "Must be POST method" });
-    }
-
-    await fetch("http://localhost:8000/todo/create", {
-        method: 'POST',
-        body: JSON.stringify(req.body),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-      .then(res => res.json())
-      .then(json => {
-        console.log(json);
-        res.status(200).json(json);
-      })
-      .catch(e => { console.error(e.message); });
-
-    res.end();
+type Data = {
+    name: string
 }
 
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if(req.method !== "POST") {
+        return res.status(405).json({message: "Method not allowed"});
+    }
 
-//   if (req.method !== "POST") {
-//       return new Response(null, { status: 400, statusText: "Must be POST method" });
-//   }
+    console.log(req.body);
 
-//   await fetch("https://jsonplaceholder.typicode.com/todos/1")
-//     .then(res => res.json())
-//     .then(json => {
-//       console.log(json);
-//       res.status(200).json(json);
-//     })
-//     .catch(e => { console.error(e.message); });
+    const data = req.body;
+    const jsonData = JSON.stringify(data);
+    var datas = JSON.parse(jsonData);
+    // client side から受け取った値をjson化
+    console.log(datas);
+    var tags1 = datas.newTag.split(/[ ,]+/);
+    var tag2 = datas.tag.split(/[ ,]+/);
+    var tags = tags1.concat(tag2);
+    const createJsonData = {
+        "title": datas.title, // tag必須->todoの作成
+        "completed": false,
+        "user": 'tester',
+        "tag": tags,
+        "created": new Date().toISOString(),
+        "updated": new Date().toISOString()
+    }
 
-//   res.end();
-// }
+    console.log(data);
+    fetch("http://localhost:8000/todos",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(createJsonData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            res.status(200).json(data);
+        })
+        .catch(error => console.error(error));
+}
