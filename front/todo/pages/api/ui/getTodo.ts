14f9ad119f864bb3a2945f2bfbb5ba2dab0510a9
me990928 <me990928@gmail.com>
2023-10-30 12:18:20 +0900

    #90

--- front/todo/pages/api/ui/getTodo.ts
+++ front/todo/pages/api/ui/getTodo.ts
@@ -1,6 +1,26 @@
+import { time } from "console";
+import { create } from "domain";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { todo } from "node:test";
+import { useState } from "react";
+interface fastTodo {
+    todo_id: number;
+    title: string;
+    tags: Tag[];
+    completed: boolean;
+    deleted: boolean;
+    created_at: number;
+    updated_at: number;
+}
+
+interface Tag {
+    tag_id: number;
+    name: string;
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    var todos :fastTodo[] = [];
 
     if (req.method !== "GET") {
         return new Response(null, { status: 400, statusText: "Must be GET method" });
@@ -11,7 +31,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         .then(json => {
             console.log(json);
             // console.log(json[0].tags);
-            res.status(200).json(json);
+             todos = json.map((item: any) => ({
+                ...item,
+                created_at: Math.floor(new Date(item.created_at).getTime() / 1000),
+                updated_at: Math.floor(new Date(item.updated_at).getTime() / 1000),
+            }));
+
+            console.log(todos[0].title+":"+todos[0].created_at);
+            console.log( todos[1].title+":"+todos[2].created_at);
+            console.log(todos[2].title + ":" +todos[3].created_at);
+
+            todos.sort((a, b) => {
+                return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+            });
+
+
+            res.status(200).json(todos);
         })
         .catch(e => { console.error(e.message); });
 
