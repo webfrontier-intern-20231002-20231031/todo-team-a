557f9df3f63b2d004a5db3d9f29f754f7c86f16f
mz-akihiro <2000akihiromizuno@gmail.com>
2023-10-30 16:51:25 +0900

    userを作成するapiを作りました

---
+++ back/src/crud/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Session
+
+from models.user import UserModel
+from schemas.schema import UserSchemaBase
+
+
+def create(db: Session, create_user_schema: UserSchemaBase):
+    user_model = UserModel(**create_user_schema.model_dump(exclude_unset=True))
+    db.add(user_model)
+    db.commit()
+    return
