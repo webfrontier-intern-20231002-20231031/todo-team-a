7b7b7ed8fdce25e0329313283168e5ffcb7a6385
mz-akihiro <2000akihiromizuno@gmail.com>
2023-10-31 11:14:49 +0900

    ログインapiを作成しました

--- back/src/api/v1/user.py
+++ back/src/api/v1/user.py
@@ -20,7 +20,9 @@ def create(user_schema: UserSchemaBase, db: Session = Depends(database.get_db)):
 @router.post("/login")
 def login(user_schema: UserSchemaBase, db: Session = Depends(database.get_db)):
     user_account = db.query(UserModel).filter(UserModel.email == user_schema.email).first()
-    if user_account:                                                                            # 既に登録されてる場合
-        raise HTTPException(status_code=401, detail="Email already registered")                 # このメッセージもいわゆる"情報の与えすぎ"に該当する可能性あり
+    if not user_account:                                                                            # 既に登録されてる場合
+        raise HTTPException(status_code=401, detail="Invalid JSON format")
+    if user_schema.password != user_account.password:
+        raise HTTPException(status_code=401, detail="Invalid JSON format")
     user_id = user.create(db, user_schema)
     return user_id
